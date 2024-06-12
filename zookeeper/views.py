c6d348f9293d0aa73186daa94678f0f1d8b54b5f
Luka <lgurjidze@gmail.com>
2024-06-12 16:00:49 +0400

    caching pages

--- zookeeper/views.py
+++ zookeeper/views.py
@@ -2,14 +2,21 @@ from rest_framework import generics
 from django.core.cache import cache
 from rest_framework.response import Response
 from zookeeper.filters import AnimalFilter
+import requests
 from zookeeper.models import Animal, Category
 from zookeeper.serializers import AnimalSerializer, CategorySerializer
 # Create your views here.
-
+from django.views.decorators.cache import cache_page
+from django.utils.decorators import method_decorator
 
 class CategoryListView(generics.ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+    @method_decorator(cache_page(60 * 1))
+    def list(self, request, *args, **kwargs):
+        response = requests.get('https://httpbin.org/delay/2')
+        return super().list(request, *args, **kwargs)
 
 
 class CategoryDetailView(generics.RetrieveUpdateDestroyAPIView):
@@ -22,13 +29,13 @@ class AnimalListView(generics.ListCreateAPIView):
     serializer_class = AnimalSerializer
 
     def list(self, request, *args, **kwargs):
-        cache_key = 'animals_list'
-        cache_time = 60 * 5
+        query_params = request.query_params
+        cache_key = f'animals_list{query_params}'
         data = cache.get(key=cache_key)
-
         if not data:
-            animals = self.get_queryset()
-            serializer = AnimalSerializer(animals, many=True)
+            filtered_queryset = self.filter_queryset(self.get_queryset())
+            cache_time = 60 * 5
+            serializer = AnimalSerializer(filtered_queryset, many=True)
             cache.set(key=cache_key, timeout=cache_time, value=serializer.data)
             return Response(serializer.data)
         
